0642e1b2063cac8cf9a717aa6e3c453e69787023
Richard Ciampa <rciampa@csumb.edu>
2015-10-21 02:23:07 -0700

    Added the includes folder and two based files for connection to MySql

---
+++ includes/conn.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+function createSqlConn() {
+	$db_user = '';
+	$db_pass = '';
+	$db_dsn = 'mysql:host=localhost;dbname=';
+	$conn = new PDO($db_dsn, $db_user, $db_pass);
+
+	return $conn;
+}
+
+?>
---
+++ includes/genericDataAccess.inc.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * We inlcude our database connection string here.
+ * PDO is used to to allow cross-platform connections without
+ * needing to write addtional SQL code
+ * 
+ * Include our connection to the RDBMS
+ */
+include_once "conn.inc.php";
+
+/*
+ * Used to fetch a data table from the database
+ */
+function fetchAllRecords($sql, $namedParameters = array()) {
+		
+	$conn = createConn();
+
+	//Prepare the sql and execute the statment
+	$query = $conn -> prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+	$query -> execute($namedParameters);
+	//Retieve all the rows
+	$records = $query -> fetchAll(PDO::FETCH_ASSOC);
+	
+	$conn = NULL;
+	
+	return $records;
+}
+
+/*
+ * Used to fetch a record from the database
+ */
+function fetchRecord($sql, $namedParameters = array()) {
+		
+	$conn = createConn();
+
+	//Prepare the sql and execute the statment
+	$query = $conn -> prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+	$query -> execute($namedParameters);
+	//Retieve all the rows
+	$record = $query -> fetch(PDO::FETCH_ASSOC);
+	
+	$conn = NULL;
+	
+	return $record;
+}
+
+
+?>
