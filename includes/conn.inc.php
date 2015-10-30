020c46fa2e8cf19d41514dfa625c17d17e3ba3e0
Richard Ciampa <rciampa@csumb.edu>
2015-10-29 20:37:21 -0700

    Update conn.inc.php

--- includes/conn.inc.php
+++ includes/conn.inc.php
@@ -5,6 +5,7 @@ function createMySqlConn() {
 	$db_pass = '';
 	$db_dsn = 'mysql:host=localhost;dbname=';
 	$conn = new PDO($db_dsn, $db_user, $db_pass);
+	$conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	return $conn;
 }
