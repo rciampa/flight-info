dd979dc6b5e5849f21bd1e8d8fe3880ee3b31d73
Andrew John Richardson <rich7002@mlc104.csumb.edu>
2015-11-01 18:30:06 -0800

    Passenger and airlines submission - Errors, see comments

--- index.php
+++ index.php
@@ -1,66 +1,79 @@
+<?php
+include "includes/genericDataAccess.inc.php";
+?>
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Airlines</title>
-    <link rel="stylesheet" href="Styles/theme.css" media="screen" charset="utf-8">
-  </head>
-  <body>
-    <nav>
-      <h3>Aviation Info</h3>
-      <ul>
-        <a href="#" class="active"><li>Airlines</li></a>
-        <a href="passengers.php"><li>Passengers</li></a>
-        <a href="aircraft.php"><li>Aircraft</li></a>
-        <a href="gates.php"><li>Gates</li></a>
-      </ul>
-    </nav>
+<head>
+  <meta charset="utf-8">
+  <title>Airlines</title>
+  <link rel="stylesheet" href="Styles/theme.css" media="screen" charset="utf-8">
+</head>
+<body>
+  <nav>
+    <h3>Aviation Info</h3>
+    <ul>
+      <a href="#" class="active"><li>Airlines</li></a>
+      <a href="passengers.php"><li>Passengers</li></a>
+      <a href="aircraft.php"><li>Aircraft</li></a>
+      <a href="gates.php"><li>Gates</li></a>
+    </ul>
+  </nav>
 
-    <div class="content">
-      <h1>Airlines</h1>
-      <form action="#" method="get">
-        <label for="text">Search by name:</label>
-        <input type="text" name="searchText">
-        <label for="select">Order by</label>
-        <select name="order">
-          <option value="name">Name</option>
-          <option value="phone">Phone</option>
-          <option value="updated">Last Updated</option>
-        </select>
-        <input type="submit" name="submit" value="Submit">
-      </form>
+  <div class="content">
+    <h1>Airlines</h1>
+    <form action="#" method="get">
+      <label for="text">Search by name:</label>
+      <input type="text" name="searchText">
+      <label for="select">Order by</label>
+      <select name="order">
+        <option value="name">Name</option>
+        <option value="phone">Phone</option>
+        <option value="updated">Last Updated</option>
+      </select>
+      <input type="submit" name="submit" value="Submit">
+    </form>
 
-      <table>
+    <table>
+      <?php
+      $searchParams = array();
+      $query = "SELECT * FROM airline";
+
+      if(isset($_GET['searchText']) && $_GET['searchText'] != NULL){
+        $query .= " WHERE name = :name";
+        $searchParams[':name'] = $_GET['searchText'];
+      }
+
+      if(isset($_GET['order'])){
+        $query .= " ORDER BY :order";
+        $searchParams[':order'] = $_GET['order'];
+      }
+
+      $results = fetchAllRecords($query, $searchParams);
+
+      // If query produces results
+      if(isset($results[0])){
+        ?>
         <th>Name</th>
         <th>Website</th>
         <th>Phone no.</th>
         <th>Last Updated</th>
-        <tr>
-          <td>Stub Airlines</td>
-          <td>stub@stubairlines.com</td>
-          <td>+1 831 202 2020</td>
-          <td>01/01/15</td>
-        </tr>
-        <tr>
-          <td>Stub Airlines</td>
-          <td>stub@stubairlines.com</td>
-          <td>+1 831 202 2020</td>
-          <td>01/01/15</td>
-        </tr>
-        <tr>
-          <td>Stub Airlines</td>
-          <td>stub@stubairlines.com</td>
-          <td>+1 831 202 2020</td>
-          <td>01/01/15</td>
-        </tr>
-        <tr>
-          <td>Stub Airlines</td>
-          <td>stub@stubairlines.com</td>
-          <td>+1 831 202 2020</td>
-          <td>01/01/15</td>
-        </tr>
-      </table>
-    </div>
 
-  </body>
+        <?php
+        //Print results
+        foreach($results as $row){
+          echo "<tr><td>" . $row['name'] .
+          "</td><td>" . $row['website'] .
+          "</td><td>" . $row['phone'] .
+          "</td><td>" . $row['updated'] .
+          "</td></tr>";
+        }
+      }else{
+        echo "<h4>Sorry, a matching airline could not be found</h4>";
+      }
+      ?>
+    </table>
+  </div>
+
+</body>
 </html>
