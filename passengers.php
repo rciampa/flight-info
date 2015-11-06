8e37ebb47502733307c06bd4216455dd9e1dbb04
sespinoza <sespinoza@csumb.edu>
2015-11-06 12:07:21 -0800

    Update: Nav

--- passengers.php
+++ passengers.php
@@ -13,10 +13,10 @@ include "includes/genericDataAccess.inc.php";
   <nav>
     <h3>Aviation Info</h3>
     <ul>
-      <a href="index.php" ><li>Airlines</li></a>
-      <a href="#" class="active"><li>Passengers</li></a>
-      <a href="aircraft.php"><li>Aircraft</li></a>
-      <a href="gates.php"><li>Gates</li></a>
+   <li><a href="#" class="active">Airlines</a></li>
+   <li><a href="passengers.php">Passengers</a></li>
+   <li><a href="aircraft.php">Aircraft</a></li>
+   <li><a href="gates.php">Gates</a></li>
     </ul>
   </nav>
 
