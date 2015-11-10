650e6cc8a9de065ec08cd2cc05ec1feca38cadeb
andyrichardson <andy-richardson@gmx.com>
2015-11-09 21:52:07 -0800

    Database connection fix

--- includes/conn.inc.php
+++ includes/conn.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 function createMySqlConn() {
-	$db_user = '';
-	$db_pass = '';
-	$db_dsn = 'mysql:host=localhost;dbname=';
+	$db_user = 'ciam1324';
+	$db_pass = '23c160875b24d7f';
+	$db_dsn = 'mysql:host=localhost;dbname=ciam1324';
 	$conn = new PDO($db_dsn, $db_user, $db_pass);
 	$conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
